7deb081a68e81ae814f4e3f9d1c3f57c56b127c2
myambitions <daurenbekzhanov@gmail.com>
2020-02-17 04:13:08 +0600

    +image parser

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -2,7 +2,9 @@ import React, { FC } from "react";
 import { Message as IMessage } from "app/types";
 import s from "./styles.module.css";
 import YouTubeParser from "components/YouTubeParser";
-const re = /(http:|https:|)\/\/(www.)?(youtu(be\.com|\.be|be\.googleapis\.com))\/(watch\?v=|v\/)?([A-Za-z0-9._%-]*)(\&\S+)?/;
+import ImageParser from "components/ImageParser";
+const ytRe = /(http:|https:|)\/\/(www.)?(youtu(be\.com|\.be|be\.googleapis\.com))\/(watch\?v=|v\/)?([A-Za-z0-9._%-]*)(\&\S+)?/;
+const imgRe = /(https?:\/\/.*\.(?:png|jpg))/i;
 const Message: FC<Props> = ({
   from,
   text,
@@ -13,14 +15,21 @@ const Message: FC<Props> = ({
 }) => {
   const isMe = from === name;
   const time = handleTime(date);
-  const isYouTubeLink = text.match(re);
+  const isYouTubeLink = text.match(ytRe);
+  const isImageLink = text.match(imgRe);
   return (
     <div className={`${s.message} ${isMe && s.own}`}>
       <div className={s.from}>
         {isMe ? you : from}, {time}
       </div>
       <div className={s.text}>
-        {isYouTubeLink ? <YouTubeParser url={text} /> : text}
+        {isYouTubeLink ? (
+          <YouTubeParser url={text} />
+        ) : isImageLink ? (
+          <ImageParser url={text} />
+        ) : (
+          text
+        )}
       </div>
     </div>
   );
